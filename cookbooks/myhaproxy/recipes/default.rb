7611d1cd12c5936f124c69b088d1644bba02c1dd
unknown <abhijitpaul0212@gmail.com>
2022-08-10 06:14:43 +0000

    Used search and bit logic in haproxy

--- cookbooks/myhaproxy/recipes/default.rb
+++ cookbooks/myhaproxy/recipes/default.rb
@@ -10,11 +10,21 @@ haproxy_frontend 'http-in' do
   default_backend 'server_backend'
 end
 
+web_nodes = search('node', 'policy_name:company_web')
+
+server_array = []
+
+web_nodes.each do |one_node|
+  one_server = "#{one_node['cloud']['public_hostname']} #{one_node['cloud']['public_ipv4']}:80 maxconn 32"
+  server_array.push(one_server)
+end
+
 haproxy_backend 'server_backend' do
-  server [
-    'ec2-3-83-225-172.compute-1.amazonaws.com 3.83.225.172:80 maxconn 32',
-    'ec2-100-26-3-47.compute-1.amazonaws.com 100.26.3.47:80 maxconn 32'
-  ]
+  # server [
+  #   'ec2-3-83-225-172.compute-1.amazonaws.com 3.83.225.172:80 maxconn 32',
+  #   'ec2-100-26-3-47.compute-1.amazonaws.com 100.26.3.47:80 maxconn 32'
+  # ]
+  server server_array
 end
 
 haproxy_service 'haproxy' do
